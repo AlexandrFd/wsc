ccbf47068cc2dadc2d8de19687221f7e1c4c546a
Aleksandr Fedotov <alexandr.fd@gmail.com>
2021-04-24 10:15:07 +0300

    First commit

---
+++ CUrl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebSiteChecker
+{
+    class CUrl
+    {
+        public string Url;
+        public double Timing;
+
+        public CUrl(string url, double timing)
+        {
+            Url = url;
+            Timing = timing;
+        }
+    }
+}
